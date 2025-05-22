1afceba35b7556007b7f5b3132212fdfbbdb211a
Kevinjj65 <kevinjohnsjolly681@gmail.com>
2025-05-22 07:30:39 +0530

    updated the contacts page

---
+++ src/parseConfig.tsx
@@ -0,0 +1,9 @@
+// utils/Parse.ts
+// @ts-ignore: No type declarations for this file
+import Parse from 'parse/dist/parse.min.js';
+
+Parse.initialize("t0e974GX3AangvOy5n5aZsIrlidERjIjuTzBv2RT", "koiFLZISXRks8kznjIQkFk7HdoeZs27jFoSvmIaH");
+Parse.serverURL = "https://parseapi.back4app.com";
+
+export default Parse;
+    
